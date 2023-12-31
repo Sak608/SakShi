fa4e38ba657a94d9b5fada5f61ed1fe8238699fa
SAKSHI RAJ <115283283+Sak608@users.noreply.github.com>
2023-12-31 14:38:02 +0530

    Update about me

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Sakshi Raj",
+  title: "Programmer & Web Designer ",
+  email: "sakshiraj708@gmail.com",
+  gitHub: "sak608",
+  instagram: "_sakshi.raj_",
+  linkedIn: "Sakshi Raj",
+  medium: "Sakshi Raj",
+  //twitter: "microsoft",
+  //youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
